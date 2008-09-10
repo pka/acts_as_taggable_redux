3c60eb4f79b1cb82c8d321e0d01d8bbfb54ccc85
Pirmin Kalberer <pka@sourcepole.ch>
2008-09-10 22:04:42 +0200

    counter_cache option

--- lib/acts_as_taggable_helper.rb
+++ lib/acts_as_taggable_helper.rb
@@ -8,19 +8,20 @@ module ActsAsTaggableHelper
   #
   # Inspired by http://www.juixe.com/techknow/index.php/2006/07/15/acts-as-taggable-tag-cloud/
   def tag_cloud(options = {})
-    options.assert_valid_keys(:limit, :conditions, :sort)
-    options.reverse_merge! :limit => 100, :sort => :name
+    options.assert_valid_keys(:limit, :conditions, :counter_cache, :sort)
+    options.reverse_merge! :limit => 100, :counter_cache => :taggings_count, :sort => :name
+    taggings_count = options.delete(:counter_cache)
     sort = options.delete(:sort)
 
-    tags = Tag.find(:all, options.merge(:order => 'taggings_count DESC')).sort_by(&sort)
+    tags = Tag.find(:all, options.merge(:order => "#{taggings_count} DESC")).sort_by(&sort)
     
     # TODO: add option to specify which classes you want and overide this if you want?
     classes = %w(popular v-popular vv-popular vvv-popular vvvv-popular)
     
     max, min = 0, 0
     tags.each do |tag|
-      max = tag.taggings_count if tag.taggings_count > max
-      min = tag.taggings_count if tag.taggings_count < min
+      max = tag.send(taggings_count) if tag.send(taggings_count) > max
+      min = tag.send(taggings_count) if tag.send(taggings_count) < min
     end
     
     divisor = ((max - min) / classes.size) + 1
@@ -29,7 +30,7 @@ module ActsAsTaggableHelper
     html <<   %(  <ul class="popularity">\n)
     tags.each do |tag|
       html << %(    <li>)
-      html << link_to(tag.name, tag_url(tag), :class => classes[(tag.taggings_count - min) / divisor]) 
+      html << link_to(tag.name, tag_url(tag), :class => classes[(tag.send(taggings_count) - min) / divisor]) 
       html << %(</li> \n)
     end
     html <<   %(  </ul>\n)
